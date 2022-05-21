1442322f016ef3372db6bd661db87c3b9cdd15ba
klld <98587362+klldme@users.noreply.github.com>
2022-05-21 12:01:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import SilverBotFN
+intro = Fore.LIGHTGREEN_EX + """
+██╗░░██╗ ██╗░░░░░██╗░░░░░██████╗
+██║░██╔╝ ██║░░░░░██║░░░░░██╔══██╗
+█████═╝  ██║░░░░░██║░░░░░██║░░██║
+██╔═██╗  ██║░░░░░██║░░░░░██║░░██║
+██║░╚██╗ ██║░░░░░██║░░░░░██████╔╝
+╚═╝░░╚═╝╚══════╝╚══════╝╚═════╝
+"""
+
+print(intro)
+
+response = requests.get("https://cdn2.klld.tk/api/v1/status")
+patch = response.json()["currentFortniteVersion"]
+
+response = requests.get("https://cdn2.klld.tk/api/v1/status")
+LOAD = response.json()["LOAD"]
+
+
+
+print(f'{LOAD}.\n')
+print(f'{patch}.\n')
